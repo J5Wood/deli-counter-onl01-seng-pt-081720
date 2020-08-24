5eaeb93c59b5de9de04a2664e7ee4a567b826472
Jeremy Wood <js.wood206@gmail.com>
2020-08-24 20:30:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,10 +4,10 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
-  elsif katz_deli.length > 0 
+  elsif katz_deli.length > 0
     counter = 1
     new_list
-    katz_deli.each do |x| 
+    katz_deli.each do |x|
       new_list << " #{counter}. #{x}"
       counter += 1
     end

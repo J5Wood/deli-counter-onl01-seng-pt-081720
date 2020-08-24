c9d56912c76b92553945af1d177d2477d3d27f07
Jeremy Wood <js.wood206@gmail.com>
2020-08-24 21:06:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,3 +22,13 @@ def take_a_number(katz_deli, name)
   line_position = katz_deli.length
   puts "Welcome, #{name}. You are number #{line_position} in line."
 end
+
+def now_serving(katz_deli)
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    currently_serving = katz_deli[0]
+    puts "Currently serving #{currently_serving}."
+    katz_deli = katz_deli.shift
+  end
+end

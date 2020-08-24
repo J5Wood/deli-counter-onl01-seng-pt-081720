8a6ef29c94bf83be5fd944c65db7e4320e0f7ffa
Jeremy Wood <js.wood206@gmail.com>
2020-08-24 20:46:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,11 +6,13 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
+    new_list
     counter = 1
     katz_deli.each do |x|
-      x.unshift(counter)
-      counter+=1
-    binding.pry
+      new_list << " #{counter}. #{x}"
     end
+    binding.pry
+    puts "The line is currently: #{new_list}"
+    
   end
 end

936147f47631130f2eff70c0af4b2593df0243ff
Jeremy Wood <js.wood206@gmail.com>
2020-08-24 20:41:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,6 +6,11 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
+    counter = 1
+    katz_deli.each do |x|
+      x.unshift(counter)
+      counter+=1
     binding.pry
+    
   end
 end
